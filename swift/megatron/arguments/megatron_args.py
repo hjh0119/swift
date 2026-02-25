2f34cc77cb15e577420f731b80602cbe8769df3f
Jintao <huangjintao.hjt@alibaba-inc.com>
2026-02-25 23:38:19 +0800

    [megatron] add micro_batch_size check (#8103)

--- swift/megatron/arguments/megatron_args.py
+++ swift/megatron/arguments/megatron_args.py
@@ -645,6 +645,11 @@ class MegatronArguments(RLHFMegatronArgumentsMixin, MegatronTunerMixin):
         self.data_parallel_size = self.world_size // total_model_size
         # Gradient Accumulation
         self.num_microbatches = self.global_batch_size // self.data_parallel_size // self.micro_batch_size
+        if self.num_microbatches == 0:
+            raise ValueError('global_batch_size must be >= `data_parallel_size * micro_batch_size` '
+                             f'to have at least one micro-batch. global_batch_size: {self.global_batch_size}, '
+                             f'data_parallel_size: {self.data_parallel_size}, '
+                             f'micro_batch_size: {self.micro_batch_size}.')
 
     def _init_teacher_model(self):
         if self.teacher_model is None:
